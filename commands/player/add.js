b74123a3d46067add092dde1b056b6936ef621ed
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-04-24 16:52:38 +0200

    add initDB file

--- commands/player/add.js
+++ commands/player/add.js
@@ -7,7 +7,6 @@ var jsonConfig = JSON.parse(configs);
 var url = 'mongodb://' + jsonConfig.mongodb + ':27017/unibot';
 
 function add(discorduser, game, server, name, channel, user){
-    console.log(user);
     //var id = user.id;
     //query init
     var query = {};
---
+++ commands/player/initPlayerTable.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord.js-commando');
+const fs = require("fs");
+const util = require('../general/util');
+var MongoClient = require('mongodb').MongoClient;
+var configs = fs.readFileSync("config.json");
+var jsonConfig = JSON.parse(configs);
+var url = 'mongodb://' + jsonConfig.mongodb + ':27017/unibot';
+
+function init(msg){
+    var playersCollection = msg.guild.members.array();
+    console.log(playersCollection);
+    return true;
+}
+
+//Analyze chat message part
+module.exports = class SayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'initPlayersDataBase',
+            group: 'player',
+            memberName: 'initDataBase',
+            description: '',
+            examples: ['']
+        });
+    }
+
+	run(msg){
+		init(msg);
+	}
+}

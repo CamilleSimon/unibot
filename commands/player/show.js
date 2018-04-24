0a68bef77db2c199ac49978225169ce030556434
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-04-24 11:48:52 +0200

    test

--- commands/player/show.js
+++ commands/player/show.js
@@ -24,8 +24,10 @@ function onePlayer(name, channel){
 						msg += "**" + result[attr] + "**```";
 					if (attr == "characters"){
 						characters = result[attr];
-						for(character in characters){
-							console.log(characters[character]);
+						for(charIndex in characters){
+							character = characters[charIndex];
+							console.log(character);
+							console.log(discorduser.get)
 							/*time = result[attr][day];
 							if(time){
 								if(time == "available" || time == "unavailable")
@@ -103,7 +105,7 @@ module.exports = class SayCommand extends Command {
 	            {
 	                key: 'discorduser',
 	                prompt: ' whom characters do you want to see ? \n De quel joueur voulez-vous voir les personnages ?',
-	                type: 'string',
+	                type: 'User',
 	            }
         	]
         });

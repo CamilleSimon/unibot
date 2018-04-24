8530d2d4a5ba952bba3e097fc241edfdded5c2a9
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-04-24 14:58:47 +0200

    test

--- commands/player/add.js
+++ commands/player/add.js
@@ -92,6 +92,7 @@ module.exports = class SayCommand extends Command {
         name = name.toLowerCase();
 		console.log("Command : playeradd, author : " + msg.author.lastMessage.member.nickname + ", arguments : " + game + ", " + server + ", " + name);
         add(discorduser, game, server, name, msg);
+        console.log(msg.guild.members);
         //TODO Check valide characters => https://scotch.io
         //if(game == "wow")
         //    return msg.say("https://worldofwarcraft.com/fr-fr/character/" + server + "/" + name);

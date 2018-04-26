dd73aca0c340675ced175400ffb58a713115500c
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-04-26 14:33:34 +0200

    adapt playershow all

--- commands/player/show.js
+++ commands/player/show.js
@@ -11,7 +11,7 @@ function onePlayer(name, channel){
 	var msg = "";
 	var player;
 	var query = {};
-	query["discordId"] = util.snowflakeToID(name);
+	query["discordId"] = util.snowflakeToID(name)
 	console.log(query);
 	MongoClient.connect(url, function(err, db) {
 	  	if (err) throw err;
@@ -57,36 +57,29 @@ function all(channel){
 	  	if (err) throw err;
 	  	db.collection("players").find({}).toArray(function(err, result) {
 			if (err) throw err;
-			console.log(result);
 	    	for(var i in result){
-				console.log(result[i]);
 	    		player = result[i];
 				for(attr in player){
-					if (attr == "discord-user"){
-						msg += "**" + player[attr] + "**```";
-						msg += "Game       | Server     | Character\n";
-						msg += "-----------+------------+--------------------------\n";
-					}
-					if (attr == "characters"){
-						characters = player[attr];
-						for(charIndex in characters){
-							character = characters[charIndex];
-							//console.log(server.members.get("id", name))
-							for(field in character){
-								var temp = util.capsFirstLetter(character[field]);
-								if (temp.length > 10)
-									msg += temp.substring(0,11);
-								else
-									msg += temp;
-								for(var i = character[field].length; i < 10; i++){
-									msg += " ";
-								}
-								if(field != "name")
-									msg += " | ";
-								else
-									msg += "\n";
-							}
+					msg += "**" + util.capsFirstLetter(result["user"]) + "** *" + util.capsFirstLetter(result["nickname"]) + "*```";
+				msg += "Game       | Server     | Character\n";
+				msg += "-----------+------------+--------------------------\n";
+				characters = result["characters"];
+				for(charIndex in characters){
+					character = characters[charIndex];
+					//console.log(server.members.get("id", name))
+					for(field in character){
+						var temp = util.capsFirstLetter(character[field]);
+						if (temp.length > 10)
+							msg += temp.substring(0,11);
+						else
+							msg += temp;
+						for(var i = character[field].length; i < 10; i++){
+							msg += " ";
 						}
+						if(field != "name")
+							msg += " | ";
+						else
+							msg += "\n";
 					}
 				}
 				msg += "```";

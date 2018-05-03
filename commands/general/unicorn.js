bb9609183065c97d13225d8b4b0baa761df8b811
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-05-03 09:42:26 +0200

    fix

--- commands/general/unicorn.js
+++ commands/general/unicorn.js
@@ -12,7 +12,7 @@ module.exports = class ReplyCommand extends Command {
         });
     }
     run(msg) {
-        fs.readFile('index.js', 'utf8', function (err,data) {
+        fs.readFile('commands/general/draw', 'utf8', function (err,data) {
             if (err) {
                 return console.log(err);
             }

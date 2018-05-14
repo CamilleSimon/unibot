ff384b305ff8ae45fe21456ad8cbcafd622b6faf
Camille Simon <37064032+CamilleSimon@users.noreply.github.com>
2018-05-14 15:45:25 +0200

    add case of unexistant character

--- commands/wow/profile.js
+++ commands/wow/profile.js
@@ -24,36 +24,38 @@ function profile(server, name, channel){
     r['name'] = name;
     bnet.wow.character.aggregate(r, function(error, response, body) {
         if(error) throw error;
-        character = response;
-        var spe;
-        switch(character.talents[0].spec.role) {
-            case "HEALING" : 
-                spe = "<:HEALING:445549335132766208>"
-                break;
-            case "DPS" :
-                spe ="<:DPS:445554272877412352>"
-                break;
-            default :
-                spe = "<:TANKING:445554088852193280>"
-        }
-        const embed = {
-            "description": "**Serveur** : " + character.realm + 
-            "\n**Niveau** : " + character.level + 
-            "\n**Classe** : " + classTab[character.class] + 
-            "\n**Niveau d'objects moyen **: " + character.items.averageItemLevel +
-            "\n**Spécialité : **" + character.talents[0].spec.name + " " + spe,
-            "color": colorTab[character.class],
-            "timestamp": new Date(),
-            "thumbnail": {
-                "url": "https://render-eu.worldofwarcraft.com/character/" + character.thumbnail
-            },
-            "author": {
-                "name": character.name,
-                "url": "https://worldofwarcraft.com/fr-fr/character/"  + character.realm  +"/" + character.name,
-                "icon_url": "https://worldofwarcraft.akamaized.net/static/components/Logo/Logo-horde.png"
+        if(response){
+            character = response;
+            var spe;
+            switch(character.talents[0].spec.role) {
+                case "HEALING" : 
+                    spe = "<:HEALING:445549335132766208>"
+                    break;
+                case "DPS" :
+                    spe ="<:DPS:445554272877412352>"
+                    break;
+                default :
+                    spe = "<:TANKING:445554088852193280>"
             }
-        }; 
-        channel.say({ embed });
+            const embed = {
+                "description": "**Serveur** : " + character.realm + 
+                "\n**Niveau** : " + character.level + 
+                "\n**Classe** : " + classTab[character.class] + 
+                "\n**Niveau d'objects moyen **: " + character.items.averageItemLevel +
+                "\n**Spécialité : **" + character.talents[0].spec.name + " " + spe,
+                "color": colorTab[character.class],
+                "timestamp": new Date(),
+                "thumbnail": {
+                    "url": "https://render-eu.worldofwarcraft.com/character/" + character.thumbnail
+                },
+                "author": {
+                    "name": character.name,
+                    "url": "https://worldofwarcraft.com/fr-fr/character/"  + character.realm  +"/" + character.name,
+                    "icon_url": "https://worldofwarcraft.akamaized.net/static/components/Logo/Logo-horde.png"
+                }
+            }; 
+            channel.say({ embed });
+        }
     });
 }
 
